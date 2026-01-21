ae80bf7a3e4456c146063f96cd1f162a76748925
Carl Isaksson <carlis@kth.se>
2026-01-21 15:18:56 +0100

    Integrated area helper function in MathHelp class
    Removed the area_calculate file and moved the function into the MathHelp file.

--- decide/src/main/java/com/decide/geometry/MathHelp.java
+++ decide/src/main/java/com/decide/geometry/MathHelp.java
@@ -13,6 +13,23 @@ public class MathHelp {
     public static double calculateDistance(Point p1, Point p2) {
         double distance = Math.pow(Math.pow(p1.x() - p2.x(), 2) + Math.pow(p1.y() - p2.y(),2),0.5);
         return distance;
-    }           
+    }
+
+    /**
+     * Calculates the area of a triangle formed by three points.
+     *
+     * @param p1 the first point
+     * @param p2 the second point
+     * @param p3 the third point
+     * @return the area of triangle formed by p1,p2,p3
+     */
+    public static double triangleArea(Point p1, Point p2, Point p3) {
+    return 0.5 * Math.abs(
+            p1.x() * (p2.y() - p3.y())
+          + p2.x() * (p3.y() - p1.y())
+          + p3.x() * (p1.y() - p2.y())
+    );
+}
+
 }
 
