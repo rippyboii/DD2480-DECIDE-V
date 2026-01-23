f30bc42ddd56c06ad8c9fb754c212e1344da87f9
Alexander Runebou <alerun@kth.se>
2026-01-23 15:58:25 +0100

    test: add unit tests #17

---
+++ decide/src/main/java/com/decide/LICs/LIC5.java
@@ -0,0 +1,14 @@
+package com.decide.LICs;
+
+import com.decide.model.Parameters;
+import com.decide.model.Point;
+
+public final class LIC5 implements LIC {
+
+    public LIC5() {}
+
+    @Override
+    public boolean evaluate(Point[] points, Parameters params) {
+        return false;
+    }    
+}
