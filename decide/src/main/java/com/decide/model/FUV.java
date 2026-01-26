0a98bfb8681822d7f4224b1f632d6ed5930b6cf4
Fabian Williams <fabian.williams@gmail.com>
2026-01-26 13:37:32 +0100

    feat: function to calculate FUV
    add unit tests for FUV
    Closes issue #51

---
+++ decide/src/main/java/com/decide/model/FUV.java
@@ -0,0 +1,34 @@
+package com.decide.model;
+
+public class FUV {
+    boolean [] puv;
+    boolean [][] pum;
+    boolean [] fuv;
+
+    public FUV(boolean [] puv, boolean [][] pum){
+        this.puv = puv;
+        this.pum = pum;
+    }
+
+    public void calculate_FUV(){
+
+        int n = this.puv.length;
+        boolean [] fuv = new boolean [n];
+        boolean row_true;
+
+        //Evaluate every row of PUV and PUM
+        for (int i = 0; i < n; i++){
+            row_true = true;
+            for (int j = 0; j < n; j++){
+                //Check for false boolean in PUM row
+                if(!this.pum[i][j]){
+                    row_true = false;
+                }
+            }
+            if(!this.puv[i] || row_true){
+                fuv[i] = true;
+            }
+        }
+        this.fuv = fuv;
+    }
+}
---
+++ decide/src/test/java/com/decide/model/FUVTest.java
@@ -0,0 +1,94 @@
+package com.decide.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FUVTest {
+    @Test
+    public void testNoTrueRowsFUV() {
+        /**
+         * Contract:
+         * FUV[i] should be set to true if PUV[i] is 
+         * false (indicating that the associated LIC should not hold back launch) or if all elements 
+         * in PUM row i are true.
+         * Expected Behavior:
+         * With an agreeing PUV and PUM the FUV should have a corresponding boolean value
+         */
+        
+        
+        //Dummy PUV
+        boolean [] puv = new boolean[]{false, true, true};
+        //Dummy PUM
+        boolean [][] pum = new boolean[][]{
+            {true, true, true},
+            {true, false, false},
+            {true, false, false}};
+        FUV fuv = new FUV(puv, pum);
+        fuv.calculate_FUV();
+
+        assertTrue(fuv.fuv[0]);
+        assertFalse(fuv.fuv[1]);
+        assertFalse(fuv.fuv[2]);
+        
+    }
+    
+    @Test
+    public void testTruePUMFUV() {
+        /**
+         * Contract:
+         * FUV[i] should be set to true if PUV[i] is 
+         * false (indicating that the associated LIC should not hold back launch) or if all elements 
+         * in PUM row i are true.
+         * Expected Behavior:
+         * Given a PUV filled with true values and a PUM filled with true values the FUV should
+         * be all true
+         */
+        
+        
+        //Dummy PUV
+        boolean [] puv = new boolean[]{true, true, true};
+        //Dummy PUM
+        boolean [][] pum = new boolean[][]{
+            {true, true, true},
+            {true, true, true},
+            {true, true, true}};
+        FUV fuv = new FUV(puv, pum);
+        fuv.calculate_FUV();
+
+        assertTrue(fuv.fuv[0]);
+        assertTrue(fuv.fuv[1]);
+        assertTrue(fuv.fuv[2]);
+
+        
+    } 
+
+    @Test
+    public void testFalsePUVFUV() {
+        /**
+         * Contract:
+         * FUV[i] should be set to true if PUV[i] is 
+         * false (indicating that the associated LIC should not hold back launch) or if all elements 
+         * in PUM row i are true.
+         * Expected Behavior:
+         * Given a PUV filled with false values and a PUM filled with false values the FUV should
+         * be all true
+         */
+        
+        
+        //Dummy PUV
+        boolean [] puv = new boolean[]{false, false, false};
+        //Dummy PUM
+        boolean [][] pum = new boolean[][]{
+            {false, false, false},
+            {false, false, false},
+            {false, false, false}};
+        FUV fuv = new FUV(puv, pum);
+        fuv.calculate_FUV();
+
+        assertTrue(fuv.fuv[0]);
+        assertTrue(fuv.fuv[1]);
+        assertTrue(fuv.fuv[2]);
+        
+    } 
+}
