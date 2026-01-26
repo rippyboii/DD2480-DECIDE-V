23104ee9a2ef505cd407c5989c4731caa5f0570a
kahoujo1 <kahounjosef3@gmail.com>
2026-01-26 12:41:52 +0100

    test: added tests for illegal paramters (#25)

--- decide/src/test/java/com/decide/LICs/LIC13Test.java
+++ decide/src/test/java/com/decide/LICs/LIC13Test.java
@@ -1,6 +1,7 @@
 package com.decide.LICs;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,42 @@ import com.decide.model.Parameters;
 import com.decide.model.Point;
 
 public class LIC13Test {
+    /**
+     * Contract:
+     * The LIC13 requires parameters A_PTS, B_PTS to be greater or eqaul to 1, and their sum to be less or equal to number of points minus 3.
+     * 
+     * Expected behavior:
+     * When supplied with parameter A_PTS = 0, the function shall throw Illegal Agument Exception.
+     * For number of point equal to 5, and A_PTS = 2, B_PTS = 2, the function shall throw Illegal Agument Exception.
+     */
+    @Test
+    void throwIllegalArgumentForInvalidParameters() {
+        LIC13 lic13 = new LIC13();
+        Point[] points = {new Point(0, 0),
+                        new Point(2, 3),
+                        new Point(2, 0),
+                        new Point(3, 0),
+                        new Point(3, 2)
+        };
+        Parameters params = new Parameters();
+        params.A_PTS=0;
+        params.B_PTS=2;
+        params.RADIUS1=1;
+        params.RADIUS2=1;
+        // A_PTS is 0, shall throw Illegal Argument
+        assertThrows(IllegalArgumentException.class, 
+            () -> {
+            lic13.evaluate(points, params);
+        });
+        params.A_PTS=2;
+        params.B_PTS=2;
+        assertThrows(IllegalArgumentException.class, 
+            () -> {
+            lic13.evaluate(points, params);
+        });
+    }
+
+
     /**
      * Contract:
      * LIC13 shall return false when the number of points is smaller than 5, as the condition cannot be met.
