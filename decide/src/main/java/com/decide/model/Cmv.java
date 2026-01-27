fd7d6b7e916a783848b8e89a59017677144de45c
kahoujo1 <kahounjosef3@gmail.com>
2026-01-27 19:28:57 +0100

    refactor: changed CMV to Cmv to follow convention (#52)

--- decide/src/main/java/com/decide/model/Cmv.java
+++ decide/src/main/java/com/decide/model/Cmv.java
@@ -1,27 +1,12 @@
 package com.decide.model;
-import com.decide.lics.Lic;
-import com.decide.lics.Lic0;
-import com.decide.lics.Lic1;
-import com.decide.lics.Lic10;
-import com.decide.lics.Lic11;
-import com.decide.lics.Lic12;
-import com.decide.lics.Lic13;
-import com.decide.lics.Lic14;
-import com.decide.lics.Lic2;
-import com.decide.lics.Lic3;
-import com.decide.lics.Lic4;
-import com.decide.lics.Lic5;
-import com.decide.lics.Lic6;
-import com.decide.lics.Lic7;
-import com.decide.lics.Lic8;
-import com.decide.lics.Lic9;
+import com.decide.lics.*;
 
 
-public class CMV {
-    private Lic[] LIClist;
+public class Cmv {
+    private Lic[] licList;
 
-    public CMV() {
-        this.LIClist = new Lic[] {  new Lic0(),
+    public Cmv() {
+        this.licList = new Lic[] {  new Lic0(),
                                     new Lic1(),
                                     new Lic2(),
                                     new Lic3(),
@@ -40,9 +25,9 @@ public class CMV {
     }
 
     public boolean[] evaluate(Point[] points, Parameters params) {
-        boolean[] cmv = new boolean[this.LIClist.length];
+        boolean[] cmv = new boolean[this.licList.length];
         for (int i = 0; i < cmv.length; i++) {
-            cmv[i] = this.LIClist[i].evaluate(points, params);
+            cmv[i] = this.licList[i].evaluate(points, params);
         }
         return cmv;
     }
